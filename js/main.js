f8fe6ec3d5c8fffc2416cbe732926c9765dc83a0
markie_m <marzolfmarkie@gmail.com>
2021-06-22 09:56:54 -0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// This is an example function. Trying to write a function to turn the entire div.work-samples card (each of four currently in the Work section) into a clickable event.
+
+// function myFunction() {
+//   document.getElementById("demo").innerHTML = "Hello World";
+// }
+
+// This is my attempt to write a function that will turn my work-samples cards into clickable events that will link to the example sites.
+// function myFunction() {
+//   document.getElementsByClassName("work-samples").onclick = function () {
+//     location.href = "https://markie-m.github.io/Jubilee-Austen/"; 
+//   }
+// }
